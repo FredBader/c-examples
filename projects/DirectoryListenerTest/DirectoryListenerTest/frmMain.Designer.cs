8dba83b3ed1a3325e8bdae546edbb7c5c9fcfd86
Fred Bader <Fredb@event1software.com>
2013-08-09 15:43:52 -0700

    DirectoryListenerTest

---
+++ projects/DirectoryListenerTest/DirectoryListenerTest/frmMain.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace DirectoryListenerTest
+{
+  partial class frmMain
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.titleFileName = new System.Windows.Forms.Label();
+      this.lblFileName = new System.Windows.Forms.Label();
+      this.titleAction = new System.Windows.Forms.Label();
+      this.titleDirectory = new System.Windows.Forms.Label();
+      this.textDirectory = new System.Windows.Forms.TextBox();
+      this.lblAction = new System.Windows.Forms.Label();
+      this.btnClose = new System.Windows.Forms.Button();
+      this.btnFolderBrowserDialog = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // titleFileName
+      // 
+      this.titleFileName.AutoSize = true;
+      this.titleFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.titleFileName.Location = new System.Drawing.Point(12, 61);
+      this.titleFileName.Name = "titleFileName";
+      this.titleFileName.Size = new System.Drawing.Size(63, 13);
+      this.titleFileName.TabIndex = 0;
+      this.titleFileName.Text = "File Name";
+      // 
+      // lblFileName
+      // 
+      this.lblFileName.AutoSize = true;
+      this.lblFileName.Location = new System.Drawing.Point(12, 81);
+      this.lblFileName.Name = "lblFileName";
+      this.lblFileName.Size = new System.Drawing.Size(54, 13);
+      this.lblFileName.TabIndex = 1;
+      this.lblFileName.Text = "File Name";
+      // 
+      // titleAction
+      // 
+      this.titleAction.AutoSize = true;
+      this.titleAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.titleAction.Location = new System.Drawing.Point(12, 116);
+      this.titleAction.Name = "titleAction";
+      this.titleAction.Size = new System.Drawing.Size(43, 13);
+      this.titleAction.TabIndex = 2;
+      this.titleAction.Text = "Action";
+      // 
+      // titleDirectory
+      // 
+      this.titleDirectory.AutoSize = true;
+      this.titleDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.titleDirectory.Location = new System.Drawing.Point(12, 9);
+      this.titleDirectory.Name = "titleDirectory";
+      this.titleDirectory.Size = new System.Drawing.Size(58, 13);
+      this.titleDirectory.TabIndex = 3;
+      this.titleDirectory.Text = "Directory";
+      // 
+      // textDirectory
+      // 
+      this.textDirectory.Location = new System.Drawing.Point(10, 30);
+      this.textDirectory.Name = "textDirectory";
+      this.textDirectory.Size = new System.Drawing.Size(301, 20);
+      this.textDirectory.TabIndex = 6;
+      // 
+      // lblAction
+      // 
+      this.lblAction.Location = new System.Drawing.Point(12, 135);
+      this.lblAction.Name = "lblAction";
+      this.lblAction.Size = new System.Drawing.Size(340, 32);
+      this.lblAction.TabIndex = 7;
+      this.lblAction.Text = "Action";
+      // 
+      // btnClose
+      // 
+      this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.btnClose.Location = new System.Drawing.Point(257, 170);
+      this.btnClose.Name = "btnClose";
+      this.btnClose.Size = new System.Drawing.Size(95, 23);
+      this.btnClose.TabIndex = 8;
+      this.btnClose.Text = "Exit";
+      this.btnClose.UseVisualStyleBackColor = true;
+      // 
+      // btnFolderBrowserDialog
+      // 
+      this.btnFolderBrowserDialog.AutoEllipsis = true;
+      this.btnFolderBrowserDialog.Location = new System.Drawing.Point(317, 30);
+      this.btnFolderBrowserDialog.Name = "btnFolderBrowserDialog";
+      this.btnFolderBrowserDialog.Size = new System.Drawing.Size(35, 20);
+      this.btnFolderBrowserDialog.TabIndex = 9;
+      this.btnFolderBrowserDialog.UseVisualStyleBackColor = true;
+      // 
+      // frmMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.btnClose;
+      this.ClientSize = new System.Drawing.Size(364, 206);
+      this.Controls.Add(this.btnFolderBrowserDialog);
+      this.Controls.Add(this.btnClose);
+      this.Controls.Add(this.lblAction);
+      this.Controls.Add(this.textDirectory);
+      this.Controls.Add(this.titleDirectory);
+      this.Controls.Add(this.titleAction);
+      this.Controls.Add(this.lblFileName);
+      this.Controls.Add(this.titleFileName);
+      this.Name = "frmMain";
+      this.Text = "Directory Listener Test";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label titleFileName;
+    private System.Windows.Forms.Label lblFileName;
+    private System.Windows.Forms.Label titleAction;
+    private System.Windows.Forms.Label titleDirectory;
+    private System.Windows.Forms.TextBox textDirectory;
+    private System.Windows.Forms.Label lblAction;
+    private System.Windows.Forms.Button btnClose;
+    private System.Windows.Forms.Button btnFolderBrowserDialog;
+  }
+}
+
---
+++ projects/DirectoryListenerTest/DirectoryListenerTest/frmMain.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+// This project has examples for the FileSystemWatcher (http://msdn.microsoft.com/en-us/library/system.io.filesystemwatcher.aspx) and
+// making safe thread calls to form controls (http://msdn.microsoft.com/en-us/library/vstudio/ms171728(v=vs.100).aspx)
+
+namespace DirectoryListenerTest
+{
+  public partial class frmMain : Form
+  {
+
+    // This delegate enables asynchronous calls for setting
+    // the text property on a TextBox and Label control.
+    #region Thread Delegate
+    delegate void SetTextCallback(string text);
+    #endregion
+
+    #region Constructor and Destructor
+    // Create a new FileSystemWatcher
+    private FileSystemWatcher omFileSystemWatcher = new FileSystemWatcher();
+
+    // Create a FolderBrowserDialog.  This will allow a user to select which
+    // to watch
+    FolderBrowserDialog omFolderBrowserDialog;
+
+    public frmMain()
+    {
+      InitializeComponent();
+      zzInitialize();
+    }
+    ~frmMain()
+    {
+      omFileSystemWatcher.EnableRaisingEvents = false;
+      omFileSystemWatcher = null;
+      omFolderBrowserDialog = null;
+    }
+
+    #endregion
+
+    // Initializes the entire form and object properties and events
+    #region Initializers
+
+    /// <summary>
+    /// Initialize the form properties, set defaults for TextBox and Labels
+    /// and initialize watchers and events
+    /// </summary>
+    private void zzInitialize()
+    {
+      zzInitFormProperties();
+      zzSetDirectoryText(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+      zzSetLabelText("", "");
+      zzSetActionListeners();
+      zzInitFolderBrowser();
+    }
+
+    /// <summary>
+    /// Set default properties of this form
+    /// </summary>
+    private void zzInitFormProperties()
+    {
+      this.FormBorderStyle = FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.Text = "File Watcher Test";
+      
+    }
+
+    /// <summary>
+    /// Initialize the FolderBrowserDialog
+    /// </summary>
+    private void zzInitFolderBrowser()
+    {
+      omFolderBrowserDialog = new FolderBrowserDialog();
+      omFolderBrowserDialog.ShowNewFolderButton = false;
+      omFolderBrowserDialog.Description = "Select the folder to watch.";
+    }
+
+    /// <summary>
+    /// Initialize event handling for the FileSystemWatcher, textDirectory TextBox,
+    /// the btnClose Button and the btnFolderBroswerDialog Button
+    /// </summary>
+    private void zzSetActionListeners()
+    {
+      // Set the directory to watch and start watching
+      zzSetDirectoryListener(textDirectory.Text);
+
+      // Watch for the textDirectory text to change and
+      // then start watching that directory
+      textDirectory.TextChanged += (sender, e) => { zzSetDirectoryListener(textDirectory.Text); };
+
+      // btnClose was clicked so close this application
+      btnClose.Click += (sender, e) => { this.Close(); };
+
+      // btnFolderBrowserDialog was clicked.  Run method to show dialog.
+      btnFolderBrowserDialog.Click += (sender, e) => { zzFolderBrowserDialog(); };
+    }
+
+    #endregion
+
+    // These methods demonstrates a pattern for making thread-safe
+    // calls on a Windows Forms control. 
+    //
+    // If the calling thread is different from the thread that
+    // created the TextBox control, these methods creates a
+    // SetTextCallback and calls itself asynchronously using the
+    // Invoke method.
+    //
+    // If the calling thread is the same as the thread that created
+    // the TextBox control, the Text property is set directly. 
+    #region Thread-safe methods
+
+    /// <summary>
+    /// Thread safe method to set the text of the lblFileName Label
+    /// </summary>
+    /// <param name="svFileName">The string value to display in the lblFileName Label</param>
+    private void zzSetFileName(String svFileName)
+    {
+
+      // InvokeRequired required compares the thread ID of the
+      // calling thread to the thread ID of the creating thread.
+      // If these threads are different, it returns true.
+      if (lblFileName.InvokeRequired)
+      {
+        SetTextCallback d = new SetTextCallback(zzSetFileName);
+        this.Invoke(d, new object[] { svFileName });
+      } 
+      else 
+      {
+        lblFileName.Text = svFileName;
+      }
+
+    }
+
+    /// <summary>
+    /// Thread safe method to set the text of the lblAction Label
+    /// </summary>
+    /// <param name="svAction">The string value to display in the lblAction Label</param>
+    private void zzSetAction(String svAction)
+    {
+
+      if (lblAction.InvokeRequired)
+      {
+        SetTextCallback d = new SetTextCallback(zzSetAction);
+        this.Invoke(d, new object[] { svAction });
+      }
+      else
+      {
+        lblAction.Text = svAction;
+      }
+
+    }
+
+    /// <summary>
+    /// Thread safe method to set the text of the textDirectory TextBox
+    /// </summary>
+    /// <param name="svPath">The text to display in the textDirectory TextBox</param>
+    private void zzSetDirectoryText(String svPath)
+    {
+      if (textDirectory.InvokeRequired)
+      {
+        SetTextCallback d = new SetTextCallback(zzSetDirectoryText);
+        this.Invoke(d, new object[] { svPath });
+      }
+      else
+      {
+        textDirectory.Text = svPath;
+      }
+    }
+    #endregion
+
+    // Listens to the file system change notifications 
+    // and raises events when a directory, or file in 
+    // a directory, changes.
+    // Event Handlers for file changes or renames
+    #region FileSystemWatcher and Event Handlers
+    private void zzSetDirectoryListener(String svDirectory)
+    {
+      try
+      {
+        // Set the path for the FileSystemWatcher to watch
+        omFileSystemWatcher.Path = svDirectory;
+
+        // Watch for changes in LastAccess and LastWrite times, and
+        // the renaming of files or directories.
+        omFileSystemWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+          | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+        // Only watch Excel files.
+        omFileSystemWatcher.Filter = "*.*";
+
+        // Add event handlers for file notifications.
+        omFileSystemWatcher.Changed += new FileSystemEventHandler(OnChanged);
+        omFileSystemWatcher.Created += new FileSystemEventHandler(OnChanged);
+        omFileSystemWatcher.Deleted += new FileSystemEventHandler(OnChanged);
+        omFileSystemWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+        // Begin watching.
+        omFileSystemWatcher.EnableRaisingEvents = true;
+
+      }
+      catch (Exception)
+      {
+        return;
+
+      }
+    }
+
+    /// <summary>
+    /// Event Handler for when the watcher detects a file has been
+    /// added, deleted or changed
+    /// </summary>
+    /// <param name="source">object</param>
+    /// <param name="e">FileSystemEventArgs</param>
+    private void OnChanged(object source, FileSystemEventArgs e)
+    {
+      // Specify what is done when a file is changed, created, or deleted.
+      String slFile = e.Name;
+      String slAction = "File " + slFile + " was: " + e.ChangeType;
+      zzSetLabelText(slFile, slAction);
+    }
+
+    /// <summary>
+    /// Event Handler for when the watcher detects a file's name has changed
+    /// </summary>
+    /// <param name="source">Object</param>
+    /// <param name="e">RenamedEventArgs</param>
+    private void OnRenamed(object source, RenamedEventArgs e)
+    {
+      // Specify what is done when a file is renamed.
+      String slFile = e.Name;
+      String slAction = "File " + e.OldName + " renamed to " + e.Name;
+      zzSetLabelText(slFile, slAction);
+    }
+    #endregion
+
+    // Helper Functions for changing Labels and Textboxes
+    #region Functions
+    /// <summary>
+    /// Sets both the File and Action label Text.
+    /// </summary>
+    /// <param name="svFile">The name of the file changed or renamed</param>
+    /// <param name="svAction">The action taken on the file.</param>
+    private void zzSetLabelText(String svFile, String svAction)
+    {
+      zzSetAction(svAction);
+      zzSetFileName(svFile);
+    }
+
+    /// <summary>
+    /// Shows the FolderDialog so a user can pick which folder to
+    /// watch for file changes.
+    /// </summary>
+    private void zzFolderBrowserDialog()
+    {
+      DialogResult olResult = omFolderBrowserDialog.ShowDialog();
+      if (olResult == DialogResult.OK)
+      {
+        zzSetDirectoryText(omFolderBrowserDialog.SelectedPath);
+        System.Diagnostics.Process.Start(omFolderBrowserDialog.SelectedPath);
+      }
+    }
+    #endregion
+  }
+}
